85c70f8acc62deedfd4d8f0b24ac113c965b5d8e
manelaskry <manlaskry8@gmail.com>
2024-04-21 03:09:12 +0200

    ineteg-auth

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -8,13 +8,26 @@ import axios from "axios";
 
 
 export default function RegisterPage() {
-  const [name,setName] = useState('');
-  const [email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [country, setCountry] = useState('');
+  const [phone, setPhone] = useState('');
 
-  function registerUser(ev){
+  async function registerUser(ev) {
     ev.preventDefault();
-    axios.get('/test')
+    try {
+    axios.post('/api/auth/register', {
+      username,
+      email,
+      password,
+      country,
+      phone
+    })
+    alert('Registration successful. Now you can log in');
+    } catch (e) {
+      alert('Registration failed. Please try again later');
+    }
   
   }
   
@@ -23,21 +36,36 @@ export default function RegisterPage() {
           <div className="mb-64">
             <h1 className="text-4xl text-center mb-4">Register</h1>
             <form className="max-w-md mx-auto" onSubmit={registerUser}>
-              <input type="text"
-                     placeholder="John Doe"
-                     value={name}
-                     onChange={ev => setName(ev.target.value)} 
-                     />
-              <input type="email"
-                     placeholder="your@email.com"
-                     value={email}
-                     onChange={ev => setEmail(ev.target.value)} 
-                     />
-              <input type="password"
-                     placeholder="password"
-                     value={password}
-                     onChange={ev => setPassword(ev.target.value)}
-                      />
+            <input
+                 type="text"
+                 placeholder="Username"
+                 value={username}
+                 onChange={ev => setUsername(ev.target.value)}
+          />
+          <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={ev => setEmail(ev.target.value)}
+          />
+          <input
+                 type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={ev => setPassword(ev.target.value)}
+          />
+          <input
+                  type="text"
+                  placeholder="Country"
+                  value={country}
+                  onChange={ev => setCountry(ev.target.value)}
+          />
+          <input
+                  type="text"
+                  placeholder="Phone"
+                  value={phone}
+                  onChange={ev => setPhone(ev.target.value)}
+          />
               <button className="primary">Register</button>
               <div className="text-center py-2 text-gray-500">
                 Already a member? <Link className="underline text-black" to={'/login'}>Login</Link>
